52362a1ed909cb56c4b2ddb2513863d17477ee5e
ikwfahmi <mikhwanfahmi4@gmail.com>
2026-09-25 10:35:52 +0700

    X00TD: Bringup @ProjectInfinity-X
    Change-Id: I71139f449d2e1db7fd0ee25d22938bcd0755dc30
    Signed-off-by: ikwfahmi <mikhwanfahmi4@gmail.com>

--- infinity_X00TD.mk
+++ infinity_X00TD.mk
@@ -10,13 +10,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+# Maintainer Name
+INFINITY_MAINTAINER := Kyura
+
+# Whether Including Google Apps
+WITH_GAPPS := false
+
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X00TD
+PRODUCT_NAME := infinity_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
